6f672edcbc2845407b86821824d3e0abaee4d4e7
AmerigoBovo97 <amerigobovo@gamil.com>
2024-10-13 22:15:55 +0200

    refactor: add arrow function

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 // html variables
-const nInput = document.getElementById("nInput")
-const fizzBuzzForm = document.getElementById("fizz-buzz-form")
-const displayResult = document.getElementById("fizz-buzz-displayer")
+const nInput = document.getElementById("nInput");
+const fizzBuzzForm = document.getElementById("fizz-buzz-form");
+const displayResult = document.getElementById("fizz-buzz-displayer");
+let newTableRow = (resultValue) => `<tr><td>${resultValue}</td></tr>`;
 
 // logic variables
-const fizzDivisor = 3
-const buzzDivisor = 5
+const fizzDivisor = 3;
+const buzzDivisor = 5;
+const fizz = "Fizz!";
+const buzz = "Buzz!!";
+const fizzbuzz = "FizzBuzz!!!";
+
+
 
 
 
 // logic
 fizzBuzzForm.addEventListener("submit", function(event){
     
-    event.preventDefault()
-    
+    event.preventDefault();
+
     for(let i = 1; i <= Number(nInput.value); i++){
         
         if(i % (fizzDivisor * buzzDivisor) === 0){
-            displayResult.innerHTML += `<tr><td>FizzBuzz!!!</td></tr>`
+            displayResult.innerHTML += newTableRow(fizzbuzz);
         }else if(i % fizzDivisor === 0){
-            displayResult.innerHTML += `<tr><td>Fizz!!</td></tr>`
+            displayResult.innerHTML += newTableRow(fizz);
         }else if(i % buzzDivisor === 0){
-            displayResult.innerHTML += `<tr><td>Buzz!!</td></tr>`
+            displayResult.innerHTML += newTableRow(buzz);
         }else{
-            displayResult.innerHTML += `<tr><td>${i}</td></tr>`
+            displayResult.innerHTML += newTableRow(i);
         }
 
     }
